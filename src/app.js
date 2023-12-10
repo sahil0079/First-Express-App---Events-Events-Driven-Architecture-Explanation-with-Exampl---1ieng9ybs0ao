cda34c25b57c58fe5ea8c99da8688da12bd3681c
sahil0079 <114945558+sahil0079@users.noreply.github.com>
2023-12-10 21:23:46 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -2,14 +2,16 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('', (req, res) => {
+app.get('/', (req, res) => {
   // TODO: Implement handling of the root endpoint '/'
   // Replace this comment with your code to send "Hello World!"
+  res.send('Hello World!');
 });
 
-app.get('', (req, res) => {
+app.get('/about', (req, res) => {
   // TODO: Implement handling of the '/about' endpoint
   // Replace this comment with your code to send "Welcome to about page"
+  res.send("Welcome to about page");
 });
 
 app.listen(port, () => {
